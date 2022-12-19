92f8c5d52ec7343e9e9a79f0a5b07dc4969ca8d0
Aniket Khatri <aksonipat2000@gmail.com>
2022-12-19 11:43:51 +0530

    update

---
+++ src/Pages/Scanner/Scanner.jsx
@@ -0,0 +1,126 @@
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Scanner() {
+	/* 
+  useLayoutEffect(()=>{
+	const myInput = document.getElementById("myTextField");
+	myInput.addEventListener(
+	  "keyup",
+	  debounce( helloWorld, 1000 )
+	  );
+  document.getElementById("myTextField").focus();
+
+  },[]) */
+
+	useEffect(() => {
+		document.getElementById("myTextField1").focus();
+		document.getElementById("myTextField2").addEventListener("keyup", debounce(helloWorld2, 200));
+		const myInput = document.getElementById("myTextField1");
+
+		myInput.addEventListener("keyup", debounce(helloWorld, 200));
+	}, []);
+
+	const [data, setData] = useState("");
+	const [data2, setData2] = useState("");
+
+	function sendData() {
+		const senddata = {
+			code1: data,
+			code2: data2,
+		};
+
+		axios({
+			method: "post",
+			url: `https://internal.microtek.tech:8443/v1/api/mhere/upload-barcode-master`,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: senddata,
+		})
+			.then((res) => {
+				console.log(res);
+				toast.success(res.data.message);
+				document.getElementById("myTextField1").focus();
+				setData("");
+				setData2("");
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.response.data.message);
+			});
+	}
+
+	function debounce(callback, delay) {
+		let timeout;
+		return function () {
+			clearTimeout(timeout);
+			timeout = setTimeout(callback, delay);
+		};
+	}
+
+	function helloWorld() {
+		document.getElementById("myTextField2").focus();
+		// console.log("KEY UP")
+	}
+	function helloWorld2() {
+		//console.log(data,data2);
+		//alert(JSON.stringify(data))
+		//document.getElementById("myTextField2").focus();
+		//console.log("KEY UP")
+	}
+
+	window.addEventListener("input", (e) => {
+		console.log(e);
+	});
+
+	return (
+		<div className="app">
+			{[...Array(1)].map((_, i) => {
+				return (
+					<div key={i} className="app-inner">
+						<p>1</p>
+						<input
+							value={data}
+							className="scan-input"
+							id={`myTextField${i + 1}`}
+							type="text"
+							onChange={(e) => {
+								setData(e.target.value);
+							}}
+						/>
+						<input
+							value={data2}
+							className="scan-input"
+							id={`myTextField${i + 2}`}
+							type="text"
+							onChange={(e) => {
+								setData2(e.target.value);
+							}}
+						/>
+						<button
+							onClick={() => {
+								sendData();
+							}}
+							className="scan-input-send-button">
+							Send
+						</button>
+						<button
+							onClick={() => {
+								document.getElementById("myTextField1").focus();
+							}}
+							className="scan-input-reset-button">
+							Reset
+						</button>
+					</div>
+				);
+			})}
+			<ToastContainer position="top-right" autoClose={2000} hideProgressBar={true} closeOnClick pauseOnHover draggable theme="colored" />
+		</div>
+	);
+}
+
+export default Scanner;
